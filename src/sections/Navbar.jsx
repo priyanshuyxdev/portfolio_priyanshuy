9264b15ce6e84190d94260a101d1accc722a1172
priyanshuyxdev <priyanshuyxdev@gmail.com>
2025-08-12 23:27:13 +0530

    revert

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
         className="fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2"
       >
         <div className="flex flex-col text-4xl gap-y-2 md:text-5xl lg:text-7xl">
-          {["home", "skills", "about", "work", "contact"].map(
+          {["home", "services", "about", "work", "contact"].map(
             (section, index) => (
               <div key={index} ref={(el) => (linksRef.current[index] = el)}>
                 {/* <Link
